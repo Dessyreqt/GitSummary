def1444c1127b733b144846647e2ccfb2df65d7e
David Carroll <carroll.david.s@gmail.com>
2019-12-25 00:38:14 -0600

    Add remote state

--- GitSummary.ps1
+++ GitSummary.ps1
@@ -2,19 +2,49 @@ function Get-GitDirs {
     Get-ChildItem . -Attributes Directory+Hidden -ErrorAction SilentlyContinue -Filter ".git" -Recurse
 }
 
+function Get-RemoteState {
+    param ( [string]$branch )
+    & git fetch
+    $remoteState = ""
+    [string]$upstream = git remote
+    if ($upstream.Length -gt 0) {
+        $remoteBranch = "$upstream/$branch"
+        $unpulled = (& git log --pretty=format:'%h' ..$remoteBranch | Measure-Object -Character).Characters
+        $unpushed = (& git log --pretty=format:'%h' "${remoteBranch}.." | Measure-Object -Character).Characters
+
+        if ($unpulled -ne 0) {
+            $remoteState += "v"
+        } else {
+            $remoteState += " "
+        }
+
+        if ($unpushed -ne 0) {
+            $remoteState += "^"
+        } else {
+            $remoteState += " "
+        }
+    } else {
+        $remoteState = "--"
+    }
+    
+    $remoteState
+}
+
 function Get-GitStatus {
-    param ([string]$dir)
+    param ( [string]$dir )
     Push-Location $dir
     $branch = git symbolic-ref HEAD | ForEach-Object { $_ -replace "^refs\/heads\/" }
+    $remoteState = Get-RemoteState $branch
     Pop-Location
     [PSCustomObject]@{ 
         Directory = $dir
         Branch = $branch
+        State = $remoteState
     }
 }
 
 function Write-GitStatusList {
-    param ([System.Collections.ArrayList]$gitStatus)
+    param ( [System.Collections.ArrayList]$gitStatus )
     $gitStatusList | Format-Table    
 }
 
