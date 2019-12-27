3ceb472ef879509d5b17ad09489d370846c9109d
David Carroll <carroll.david.s@gmail.com>
2019-12-27 02:23:35 -0600

    Safer way to check for alias - Add output for AddAlias

--- AddAlias.ps1
+++ AddAlias.ps1
@@ -3,13 +3,16 @@ if ((Test-Path $profile) -eq $false) {
 }
 
 $profileContent = Get-Content -Path $profile
-$aliasStr = "New-Alias GitSummary ""$PSScriptRoot\GitSummary.ps1"""
+$aliasStr = 'New-Alias GitSummary "$PSScriptRoot\GitSummary.ps1"'
 if ($null -eq $profileContent -or $profileContent.Contains($aliasStr) -eq $false) {
     Add-Content -Path $profile -Value $aliasStr
 }
 
 $aliasCmd = $aliasStr + " -Scope Global"
 
-if ($null -eq (Get-Alias GitSummary)) {
+if (!(Get-Alias GitSummary*)) {
     Invoke-Expression $aliasCmd
+    Write-Host 'Alias "GitSummary" created and added to profile.'
+} else {
+    Write-Host 'Alias "GitSummary" already exists.'
 }
