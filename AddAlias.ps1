16f27cd0fbe39a5e539df6c208852cafc6869b64
David Carroll <carroll.david.s@gmail.com>
2019-12-27 02:27:03 -0600

    Better output for AddAlias

--- AddAlias.ps1
+++ AddAlias.ps1
@@ -6,13 +6,14 @@ $profileContent = Get-Content -Path $profile
 $aliasStr = 'New-Alias GitSummary "$PSScriptRoot\GitSummary.ps1"'
 if ($null -eq $profileContent -or $profileContent.Contains($aliasStr) -eq $false) {
     Add-Content -Path $profile -Value $aliasStr
+    Write-Host 'Alias "GitSummary" added to profile.'
 }
 
 $aliasCmd = $aliasStr + " -Scope Global"
 
 if (!(Get-Alias GitSummary*)) {
     Invoke-Expression $aliasCmd
-    Write-Host 'Alias "GitSummary" created and added to profile.'
+    Write-Host 'Alias "GitSummary" created for current session.'
 } else {
-    Write-Host 'Alias "GitSummary" already exists.'
+    Write-Host 'Alias "GitSummary" already exists in current session.'
 }
